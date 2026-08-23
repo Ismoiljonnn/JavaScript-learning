6e7e1e69d2e53cfa4ee0c50d98b5f888d229f2c9
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 16:48:19 +0500

    template literals

---
+++ 06 strings/template_literals.js
@@ -0,0 +1,6 @@
+// ES6 introduced template literals for creating strings,
+// which are defined using backtick (``) characters.
+
+let firstName = 'Ismoiljon';
+let message = `Salom ${firstName}`;
+console.log(message);
