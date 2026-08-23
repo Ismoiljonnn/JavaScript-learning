8c232ec0550fe6ab1215990b4e29e66754926791
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:46:02 +0500

    determining an array length

---
+++ 10 Array/determining_array_length.js
@@ -0,0 +1,6 @@
+// Determining array length – To find the total
+// number of elements in an array, use the length property.
+
+const languages = ['js', 'java', 'go'];
+
+console.log(languages.length); // 3
