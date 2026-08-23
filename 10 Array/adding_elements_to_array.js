b4a3ae5245ccb597a23338c480f745b88ef34210
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:36:22 +0500

    adding elements to an array

---
+++ 10 Array/adding_elements_to_array.js
@@ -0,0 +1,17 @@
+// To add elements to an array, you can use the
+// push() or unshift() array methods.
+
+// push() method adds an element to the end of an array:
+let languages = ['js', 'java'];
+
+languages.push('go');
+
+console.log(languages); // ['js', 'java', 'go']
+
+// unshift() method adds an element to the beginning of an array:
+
+let languages2 = ['js', 'java'];
+
+languages2.unshift('go');
+
+console.log(languages); // ['go', 'js', 'java']
