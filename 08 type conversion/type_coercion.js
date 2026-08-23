dac8f4fb732e0264fcdea58d6d000287c946b700
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 17:30:44 +0500

    auto type conversions

---
+++ 08 type conversion/type_coercion.js
@@ -0,0 +1,11 @@
+// Type coercion – automatic conversion of a value
+// from one data type to another by JavaScript when
+// an operator acts on different value types.
+// In other words, it is the automatic transformation of
+// a data type from one form to another.
+
+// Auto string conversion
+console.log("I was born in the year " + 1997);
+
+// Auto number conversion
+console.log('30' - '10' - 5);
