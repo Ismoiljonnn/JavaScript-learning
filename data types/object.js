d3b6c2422a2a556b57151c97fb1101f6d4ac78f8
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 11:20:14 +0500

    object data type

---
+++ data types/object.js
@@ -0,0 +1,12 @@
+// Object
+// An object value is stored at another address referenced by the variable.
+
+// Function
+// Array
+// Object
+
+// Function – A reusable block of code designed to perform a specific task when invoked.
+
+// Array – An ordered collection of values stored and accessed by zero-based numerical indexes.
+
+// Object – A standalone entity storing data as a collection of key-value pairs.
