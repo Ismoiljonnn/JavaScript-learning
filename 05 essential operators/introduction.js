384d06cb8686929e84ed1b2a6de2f982b9eeecd1
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 11:53:56 +0500

    introduction to operators

---
+++ 05 essential operators/introduction.js
@@ -0,0 +1,14 @@
+// Operators perform operations on one or more operands.
+// The general syntax is as follows:
+
+// <left operand> operator <right operand> = // num1 + num2
+// <left operand> operator = // num1++
+// operator <right operand> = // --num2
+
+// The following types of operators are commonly available in JavaScript:
+
+// 1. Arithmetic operators
+// 2. Comparison operators
+// 3. Assignment operators
+// 4. Logical operators
+// 5. Ternary operator
