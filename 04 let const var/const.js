f2261ea1cc0eda4d3c4ab3eb7972c0405104cd4f
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 11:36:21 +0500

    declare variable using const

---
+++ 04 let const var/const.js
@@ -0,0 +1,6 @@
+// const (short for "constant") – used to declare variables that will
+// not change during program execution.
+
+const id = 'AA0'; // Can not change.
+
+// Variables declared using const must always have an initial value.
