8b971fce318a033b343bc0fda270f5b705cb5c31
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 11:40:47 +0500

    declare variable using var

---
+++ 04 let const var/var.js
@@ -0,0 +1,16 @@
+// var – the keyword used to declare variables prior to ES6.
+// It performed almost the same function as let.
+
+var age = 23;
+age = 24;
+
+// Although let and var are similar, they have key differences.
+// A variable declared using var is function-scoped, whereas let
+// is block-scoped.
+
+// In JavaScript, it is possible to create a variable without
+// specifying any keyword, but creating variables in this manner
+// is not recommended.
+
+user = Ismoiljon; // It is not recommended.
+console.log(user);
