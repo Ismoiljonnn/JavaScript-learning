1c097a479ceab89ecd24b690cb9b35ae5a1a7f2e
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 16:46:12 +0500

    Introduction to strings

---
+++ 06 strings/introduction.js
@@ -0,0 +1,10 @@
+// String – a data type composed of a sequence of characters.
+// Strings can be created using single ('') or double ("") quotes.
+// Double quotes are typically used when the text contains special
+// characters like apostrophes.
+
+let firstName = 'Ismoiljon';
+console.log(firstName);
+let surname = "Jo'raboyev";
+surname = 'Jo\'raboyev'; // If you want to use single quotes.
+console.log(surname);
