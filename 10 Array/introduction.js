2ec872f69c96bbf230ae73da68134230fd337a5a
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:21:06 +0500

    introduction to arrays

---
+++ 10 Array/introduction.js
@@ -0,0 +1,5 @@
+// Array – an object capable of storing multiple
+// values at the same time. Below is an array
+// named languages consisting of 4 elements.
+
+const languages = ['python', 'js', 'java', 'go'];
