1bdb20213b8336e65229a868b814790a262c649b
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:39:56 +0500

    modifying array elements

---
+++ 10 Array/modifying_array_elemts.js
@@ -0,0 +1,10 @@
+// Modifying array elements – You can change the value of
+// an existing array element by accessing its index and
+// assigning a new value to it.
+
+let languages = ['js', 'java', 'go'];
+
+// Replaces 'java' at index 1 with 'python'
+languages[1] = 'python';
+
+console.log(languages); // ['js', 'python', 'go']
