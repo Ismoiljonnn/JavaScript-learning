9009760f27d61309b41348608b0a6df8203b8d03
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 10:50:03 +0500

    JavaScript reserved words list

---
+++ values and variables/reserved.js
@@ -0,0 +1,17 @@
+// JavaScript Reserved Words & Keywords
+const jsReservedWords = [
+  // Keywords
+  "break", "case", "catch", "class", "const", "continue",
+  "debugger", "default", "delete", "do", "else", "export",
+  "extends", "false", "finally", "for", "function", "if",
+  "import", "in", "instanceof", "new", "null", "return",
+  "super", "switch", "this", "throw", "true", "try",
+  "typeof", "var", "void", "while", "with", "yield",
+
+  // Strict mode & Future reserved words
+  "implements", "interface", "let", "package", "private",
+  "protected", "public", "static", "enum", "await", "async",
+
+  // Contextual & Global objects/properties
+  "arguments", "eval", "undefined", "NaN", "Infinity"
+];
