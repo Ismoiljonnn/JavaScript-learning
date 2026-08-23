234661b65fb590d1347b1f39dd79e0292a7620eb
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:00:23 +0500

    determine a whether given number is even or odd

---
+++ 09 functions/functions practise/task1.js
@@ -0,0 +1,28 @@
+// Create a function that determines whether a given number is even or odd:
+
+// Function declaration
+function evenOrOddDec(number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is even`);
+  } else {
+    console.log(`${number} is odd`)
+  }
+}
+
+// Function expression
+const evenOrOddExp = function(number) {
+  if (number % 2 === 0) {
+    console.log(`${number} id even`);
+  } else {
+    console.log(`${number} is odd`);
+  }
+}
+
+// Arrow function
+const evenOrOddArrow = (number) => {
+  if (number % 2 === 0) {
+    console.log(`${number} is even`);
+  } else {
+    console.log(`${number} is odd`);
+  }
+}
