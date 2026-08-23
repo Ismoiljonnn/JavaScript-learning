d76879aee6e35fd5e558ef51fe5db040df215d85
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 20:26:58 +0500

    introduction to finctions

---
+++ 09 functions/introduction.js
@@ -0,0 +1,30 @@
+// Function – one of the core fundamentals of JavaScript that
+// allows executing a specific task. A function runs when it is
+// invoked (called) from another part of the code. It enables
+// code reusability, meaning you can declare it once and use it
+// in multiple places.
+
+// General syntax
+
+// function functionName(parameter1, parameter2, ...,   parameterN) {
+//   statement 1;
+//   statement 2;
+//   statement 3;
+//   return output;
+// }
+
+// functionName(argument1, argument2, ..., argumentN);
+
+// Function example
+
+function myFunction(num1, num2) {
+  return num1 + num2;
+}
+
+myFunction(10, 20); // 30 // Function call
+myFunction(20, 30); // 50 // Function call
+
+// Declaring types:
+// Function declaration
+// Function expression
+// Arrow function
