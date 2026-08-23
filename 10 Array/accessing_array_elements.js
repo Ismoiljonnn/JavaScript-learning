71b9a5ce1dbf2e5526b129754f5d794c2a85490f
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:35:06 +0500

    accessing array elements

---
+++ 10 Array/accessing_array_elements.js
@@ -0,0 +1,10 @@
+// Accessing array elements – Array elements can be
+// accessed using their index positions.
+
+const helloArray = ['h', 'e', 'l', 'l', 'o'];
+
+// First element (index 0)
+console.log(helloArray[0]); // "h"
+
+// Third element (index 2)
+console.log(helloArray[2]); // "l"
