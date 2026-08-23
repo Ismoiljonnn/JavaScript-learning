ab21698528e81c3b3a814693924f1b61d1578bfd
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 22:27:55 +0500

    creating array

---
+++ 10 Array/creating_array.js
@@ -0,0 +1,25 @@
+// Array literal notation
+// The easiest way to create an array is using an array literal,
+// which is created using square brackets ([]).
+
+const languages = ['js', 'java', 'go'];
+
+// Using the new keyword
+// In JavaScript, arrays can also be created using the new keyword.
+
+const languages2 = new Array('js', 'java', 'go');
+
+// Arrays can store elements of different data types together
+// within the same array.
+
+// Empty array
+const emptyArray = [];
+
+// Array of numbers
+const sonArray = [1, 2, 9, -2];
+
+// Array of strings
+const stringArray = ['Javascript', 'Java', 'Go'];
+
+// Array with mixed data types
+const mixedArray = ['Javascript', 2, false, null];
