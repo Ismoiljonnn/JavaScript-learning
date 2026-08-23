7925d99e78385025ffc9d1e48737028a2d1c2b64
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 12:19:36 +0500

    arithmetic operators

---
+++ 05 essential operators/arithmetic_operators.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+// In JavaScript, arithmetic operators are used to perform
+// mathematical operations on operands.
+
+// +   Adds two operands -> num1 + num2
+// -   Subtracts the right operand from the left operand -> num1 - num2
+// *   Multiplies two operands -> num1 * num2
+// /   Divides the left operand by the right operand -> num1 / num2
+// %   Modulus operator. Returns the division remainder -> num1 % num2
+// ++  Increment operator. Increases the operand value by one -> num1++ or ++num1
+// --  Decrement operator. Decreases the operand value by one -> num2-- or --num2
+
+let a = 5, b = 10;
+console.log(a + b); // 15
+console.log(a - b); // -5
+console.log(a * b); // 50
+console.log(a / b); // 0.5
+console.log(a % b); // 5
+console.log(a++);   // 5 (post-increment)
+console.log(a);     // 6
+console.log(++a);   // 7 (pre-increment)
+console.log(a);     // 7
+console.log(b--);   // 10 (post-decrement)
+console.log(b);     // 9
+console.log(--b);   // 8 (pre-decrement)
+console.log(b);     // 8
