f520ed1ae4687e1c883d22330d86a4cced63fca7
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-23 11:30:00 +0500

    declare variable using let

---
+++ 04 let const var/let.js
@@ -0,0 +1,8 @@
+// let – used to declare variables that can change during program
+// execution.
+
+let age = 23;
+age = 24;
+
+// No keyword is used when reassigning the value of an already declared
+// variable.
