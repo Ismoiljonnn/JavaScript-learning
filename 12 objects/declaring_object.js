814b04e8b5aae68d8d6b06ebf942e822afb7af5d
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:17:33 +0500

    declaring an object

---
+++ 12 objects/declaring_object.js
@@ -0,0 +1,14 @@
+// Declaring an Object – Objects are created using curly braces {},
+// where each property follows a key: value structure.
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(typeof programmer); // "object"
+
+let person = {
+  name: 'Ismoiljon', // Key: name  | Value: 'Ismoiljon'
+  age: 16            // Key: age   | Value: 16
+};
