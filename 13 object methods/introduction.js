b8f4fdf5a582bb7e633476aaa6270b8af9d7a278
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:36:57 +0500

    introduction to object methods

---
+++ 13 object methods/introduction.js
@@ -0,0 +1,11 @@
+// JavaScript Method – an object property that has a function as its value.
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16,
+  greet: function() {
+    console.log('Hello!');
+  }
+};
+
+programmer.greet(); // Output: "Hello!"
