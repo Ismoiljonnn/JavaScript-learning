08e047fc90ee276597fd780b3c80d42c35618670
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:41:20 +0500

    using object.keys() method

---
+++ 13 object methods/special methods/object_keys.js
@@ -0,0 +1,9 @@
+// Object.keys() Method – a built-in function that
+// returns an array of an object's property names (keys).
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(Object.keys(programmer)); // ['name', 'age']
