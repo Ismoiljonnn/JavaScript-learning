382cb9387d86bd5ec57defeaa8924bdd6ff2ec09
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:45:04 +0500

    using object.entries() method

---
+++ 13 object methods/special methods/object_entries.js
@@ -0,0 +1,10 @@
+// Object.entries() Method – a built-in function that returns
+// an array of an object's key-value pairs as [key, value] arrays.
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(Object.entries(programmer)); 
+// [['name', 'Ismoiljon'], ['age', 16]]
