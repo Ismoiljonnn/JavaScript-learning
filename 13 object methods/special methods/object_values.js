1a21ca334485a7087cd0751d1e7e041551199f87
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:43:04 +0500

    using object.values() method

---
+++ 13 object methods/special methods/object_values.js
@@ -0,0 +1,9 @@
+// Object.values() Method – a built-in function that
+// returns an array of an object's property values.
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(Object.values(programmer)); // ['Ismoiljon', 16]
