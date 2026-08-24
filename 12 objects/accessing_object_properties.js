c41c9d07bef67e3ee39e6c2b86a4150611eabc59
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-24 09:19:55 +0500

    accessing objectt properties

---
+++ 12 objects/accessing_object_properties.js
@@ -0,0 +1,20 @@
+// Accessing Object Properties – You can access the
+// value of an object's property using its key.
+
+// Using Dot Notation (.)
+
+const programmer = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(programmer.name); // "Ismoiljon"
+
+// Using Bracket Notation ([])
+
+const programmer2 = {
+  name: 'Ismoiljon',
+  age: 16
+};
+
+console.log(programmer2['name']); // "Ismoiljon"
