46acfb764388552dda65b0b3f0c1a7441b9fc5f0
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-06 00:02:48 +0500

    letter counting and frequency analysis

---
+++ 12 objects/practice/16 count-letters.js
@@ -0,0 +1,27 @@
+function countLetters(text) {
+  const frequency = {};
+  for (const char of text) {
+    if (char !== " ") {
+      frequency[char] = (frequency[char] || 0) + 1;
+    }
+  }
+  return frequency;
+}
+
+function findMostFrequent(frequency) {
+  let mostFrequentChar = "";
+  let maxCount = 0;
+
+  for (const char in frequency) {
+    if (frequency[char] > maxCount) {
+      maxCount = frequency[char];
+      mostFrequentChar = char;
+    }
+  }
+
+  return ${mostFrequentChar} ( times);
+}
+
+console.log(countLetters("programming"));
+console.log(countLetters("hello world"));
+console.log(findMostFrequent(countLetters("hello world")));
