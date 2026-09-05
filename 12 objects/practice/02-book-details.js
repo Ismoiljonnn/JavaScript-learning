593bdbb85a378401dad04ec2ce65192ca963ed89
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-05 22:54:24 +0500

    read and format object properties into strings

---
+++ 12 objects/practice/02-book-details.js
@@ -0,0 +1,11 @@
+const book = {
+  title: "O'tkan kunlar",
+  author: "Abdulla Qodiriy",
+  year: 1926,
+  pages: 380
+};
+
+const bookTitle = ${book.title} - ;
+const bookDetails = ${book.year}-yil,  sahifa;
+
+module.exports = { bookTitle, bookDetails };
