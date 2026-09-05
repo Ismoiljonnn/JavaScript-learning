4c04510d29933adf316361c09afbaa8065d806e2
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-06 00:02:47 +0500

    menu list calculation with single loop

---
+++ 12 objects/practice/13 menu-list.js
@@ -0,0 +1,19 @@
+const menu = { pilaf: 35000, somsa: 8000, soup: 28000, tea: 3000 };
+
+let index = 1;
+let totalCount = 0;
+let cheapestPrice = Infinity;
+
+for (const item in menu) {
+  const price = menu[item];
+  console.log(${index}.  -  sum);
+  
+  totalCount++;
+  if (price < cheapestPrice) {
+    cheapestPrice = price;
+  }
+  index++;
+}
+
+console.log("Total dishes: " + totalCount);
+console.log("Cheapest price: " + cheapestPrice);
