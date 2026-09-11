37725b72a521542d248fb9b3760a5cd2adfb584d
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-11 17:01:26 +0500

    reverse | reverses the array

--- 11 array methods/practice/methods.js
+++ 11 array methods/practice/methods.js
@@ -2,3 +2,4 @@ array.push(6); // add to at the end
 array.unshift(0); // add to the at the start
 array.pop(); // removing last element
 array.shift(); // removing first element
+array.reverse(); // reversing the array
