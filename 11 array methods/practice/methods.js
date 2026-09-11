09765f1fb74a76bfbac44467530ac626f58c3e93
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-11 17:40:27 +0500

    sort | sorts the array with given function

--- 11 array methods/practice/methods.js
+++ 11 array methods/practice/methods.js
@@ -7,3 +7,4 @@ array.includes(); // searching a value, returns true or false
 array.find((value, index) => {return value === 7;}); // returns value or undefined
 array.filter((value, index, array) => {return value % 2 === 1;}); // returns filtered values
 array.join("something"); // adds something after each element
+array.sort((a, b) => {a + b}); // sorts the array with given function
