a8185dcb1e893991d776afce7ef8a1876bffd2bf
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-14 17:03:40 +0500

    every | checks if all elements satisfy the condition

--- 11 array methods/practice/methods.js
+++ 11 array methods/practice/methods.js
@@ -15,3 +15,4 @@ array.reduce((prevValue, value, index) => {}, 0) // reduces array into a single 
 new Array(amount) // creates an empty array with 'amount' length
 array.fill(value) // fills all array elements with 'value'
 new Array(5).fill(0); // creates an array of 5 elements and fills with 0
+array.every((value) => {value > 0}); // checks if all elements satisfy the condition
