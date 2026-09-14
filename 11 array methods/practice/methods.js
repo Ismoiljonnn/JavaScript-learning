530df91f9aea1486af60b0e3a87788ebf5e72a40
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-14 16:44:01 +0500

    reduece | reduces array into one function

--- 11 array methods/practice/methods.js
+++ 11 array methods/practice/methods.js
@@ -11,3 +11,4 @@ array.sort((a, b) => {a + b}); // sorts the array with given function
 array.findIndex((value) => value === 5); // return index of array element
 array.slice(0, 5); // slices some part
 array.splice(0, 4); // deletes some part
+array.reduce((prevValue, value, index) => {}, 0) // reduces array into a single value
