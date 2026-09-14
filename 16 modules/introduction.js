a284195f841748436b25e011fc75f489ac7c43e8
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-14 17:31:20 +0500

    introduction to modules

---
+++ 16 modules/introduction.js
@@ -0,0 +1,8 @@
+// JavaScript Modules -- used to break code into separate reusable files.
+// Helps maintain clean code architecture and prevents polluting the global scope.
+// It enables code reusability, maintainability, and encapsulation across large applications.
+
+// Conceptual overview:
+// Modules allow sharing variables, functions, or classes across different files.
+// - export: makes code accessible to other files
+// - import: fetches exported code into the current file
