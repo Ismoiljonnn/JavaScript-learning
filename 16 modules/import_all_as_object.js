256a0257d9e1a11acc6bbb204a0254a67b4a4703
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-09-18 17:01:06 +0500

    import all as a single object

---
+++ 16 modules/import_all_as_object.js
@@ -0,0 +1,6 @@
+// Import All as Object - imports all named exports from a module as a single object namespace.
+
+import * as MathUtils from './named_exports.js';
+
+console.log(MathUtils.API_URL);
+console.log(MathUtils.calculateTotal(10, 20));
